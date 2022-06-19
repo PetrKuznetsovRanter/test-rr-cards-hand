f35dccab1ef5578c17bbd20efd177a7b1e045153
petr.kuznetsov <petr.kuznetsov@gerrit.aglet.app>
2022-06-19 20:28:51 +0300

    Add some standard thirdparty plugins

---
+++ CardsHand/Assets/Plugins/UniTask/Runtime/Internal/ValueStopwatch.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Diagnostics;
+
+namespace Cysharp.Threading.Tasks.Internal
+{
+    internal readonly struct ValueStopwatch
+    {
+        private static readonly double TimestampToTicks = TimeSpan.TicksPerSecond / (double) Stopwatch.Frequency;
+
+        private readonly long startTimestamp;
+
+        private ValueStopwatch(long startTimestamp)
+        {
+            this.startTimestamp = startTimestamp;
+        }
+
+        public TimeSpan Elapsed => TimeSpan.FromTicks(ElapsedTicks);
+
+        public bool IsInvalid => startTimestamp == 0;
+
+        public long ElapsedTicks
+        {
+            get
+            {
+                if (startTimestamp == 0)
+                    throw new InvalidOperationException("Detected invalid initialization(use 'default'), only to create from StartNew().");
+
+                var delta = Stopwatch.GetTimestamp() - startTimestamp;
+                return (long) (delta * TimestampToTicks);
+            }
+        }
+
+        public static ValueStopwatch StartNew()
+        {
+            return new(Stopwatch.GetTimestamp());
+        }
+    }
+}
